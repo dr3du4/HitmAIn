c8f16769052a5954ebf37397cb5bd561bd309e64
Jan Krzyszkowski <jan.krzyszkowski2@gmail.com>
2024-11-24 00:52:20 +0100

    working model

--- HitmAIn/Assets/playerAI.cs
+++ HitmAIn/Assets/playerAI.cs
@@ -3,11 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
 public class playerAI : Agent
 {
+    [SerializeField] private Transform target;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        transform.position = startPos;
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(target.position);
+    }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        Debug.Log("OnActionReceived");
+        float moveX = actions.ContinuousActions[0];
+        float moveY = actions.ContinuousActions[1];
+
+        float moveSpeed = 1f;
+        transform.position += new Vector3(moveX, moveY, 0) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> actions = actionsOut.ContinuousActions;
+        actions[0]=Input.GetAxis("Horizontal");
+        actions[1]=Input.GetAxis("Vertical");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.tag);
+        if (other.tag == "Goal")
+        {
+            SetReward(1f);
+            EndEpisode();
+        }
+        else if (other.tag == "Wall")
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+        
     }
 }
